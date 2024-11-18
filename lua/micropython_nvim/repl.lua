a473d68320e70821b0b17717094c7a206e6b5c49
James Best <jim@justjibba.net>
2024-11-18 14:40:24 +0000

    Fix repl command

--- lua/micropython_nvim/repl.lua
+++ lua/micropython_nvim/repl.lua
@@ -6,7 +6,7 @@ local M = {}
 -- The port is taken from the global variable 'AMPY_PORT'.
 -- The command used is 'rshell -p <port>'.
 function M.repl()
-  local repl_assembled_command = string.format('rshell -p %s', _G['AMPY_PORT'])
+  local repl_assembled_command = string.format('rshell -p %s repl', _G['AMPY_PORT'])
   local term = Terminal:new({ cmd = repl_assembled_command, direction = 'float' })
   term:toggle()
 end
